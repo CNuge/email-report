70b015c85e9e13d2ab0336b05b41d06108b7da3f
WATTBA <nugentc@uoguelph.ca>
2017-11-24 12:25:24 -0500

    typo fixed.

--- countdowns.py
+++ countdowns.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 	
 def day_of_year(now=datetime.now()):
-	day_of_the_year = now - datetime(now.year, 1, 1) 
+	day_of_the_year = now - datetime(now.year -1, 12, 31) 
 	date_summary = 'Today is day number %d of the year %d.\n' % (day_of_the_year.days, now.year)
 	return date_summary
 
